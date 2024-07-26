8d77696cc9b10a4b86cc681247dd9385337101cd
Gianluigi Trontini <g.trontini@nbsgroup.it>
2024-07-26 16:15:08 +0200

    Initial Commit

---
+++ SeaBnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeaBnb
+//
+//  Created by Gianluigi Trontini on 26/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeaBnb/SeaBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaBnbApp.swift
+//  SeaBnb
+//
+//  Created by Gianluigi Trontini on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SeaBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
